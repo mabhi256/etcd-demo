e0fa62771b6dd6bef98b872a1b77d2f74a7c351c
Abhishek Mandingi <mabhi256@gmail.com>
2026-05-04 13:58:08 +0530

    feat: add try lock in the locking examples

--- cmd/08-lock/main.go
+++ cmd/08-lock/main.go
@@ -53,6 +53,7 @@ func main() {
 	// -------------------------------------------
 	// Pessimistic Lock (concurrency.Mutex)
 	// Use when: Contention is expected, long operations, can't retry easily
+	// Waits to acquire the lock
 	// -------------------------------------------
 	fmt.Println("\n=== Pessimistic Lock ===")
 
@@ -75,6 +76,49 @@ func main() {
 			fmt.Printf("[Worker-%d] releasing lock\n", i)
 
 			mu.Unlock(context.Background())
+		})
+	}
+
+	wg.Wait()
+
+	// -------------------------------------------
+	// TryLock
+	// Use when: Don't wait if we didn't get the lock, do something else
+	//
+	// NewMutex is implemented using a similar mechanism, only difference is
+	// it sets up a watch on the predecessor key if it fails to acuire the lock.
+	// When the predecessor is deleted, it checks again if it has the lowest key
+	// -------------------------------------------
+	fmt.Println("\n=== TryLock ===")
+
+	lockPrefix := "/try-lock"
+
+	for i := 1; i <= 3; i++ {
+		wg.Go(func() {
+			nodeID := fmt.Sprintf("Node-%d", i)
+
+			// Each contender writes to a unique key under a shared prefix.
+			lease, _ := cli.Grant(context.Background(), 10)
+			myKey := fmt.Sprintf("%s/%d", lockPrefix, lease.ID)
+			cli.Put(context.Background(), myKey, nodeID, clientv3.WithLease(lease.ID))
+
+			// Lock holder = lowest create revision under the prefix.
+			getResp, _ := cli.Get(context.Background(), lockPrefix,
+				clientv3.WithPrefix(),
+				clientv3.WithSort(clientv3.SortByCreateRevision, clientv3.SortAscend),
+				clientv3.WithLimit(1),
+			)
+
+			lowestKey := getResp.Kvs[0]
+			if string(lowestKey.Key) == myKey {
+				fmt.Printf("[%s] Acquired lock\n", nodeID)
+				time.Sleep(1 * time.Second) // Simulate work
+			} else {
+				fmt.Printf("[%s] failed to acquire lock\n", nodeID)
+				// NewMutex watches the predecessor key (the one just before ours by create revision)
+				// When the predecessor key is deleted, NewMutex rechecks lowest key and acquire the lock
+			}
+			cli.Revoke(context.Background(), lease.ID)
 		})
 	}
 
